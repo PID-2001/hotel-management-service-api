a3e8ec8cbb00a8b50177befefb4d72ba5658192d
Pathum Isuru <pathumisuru19@gamil.com>
2026-03-09 19:58:48 +0530

    complete repo section

--- src/main/java/com/cpd/hotel_system/hotel_management_service_api/repo/FacilityRepo.java
+++ src/main/java/com/cpd/hotel_system/hotel_management_service_api/repo/FacilityRepo.java
@@ -3,6 +3,6 @@ package com.cpd.hotel_system.hotel_management_service_api.repo;
 import com.cpd.hotel_system.hotel_management_service_api.entity.Facility;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface FacilityRepo extends JpaRepository<Facility, String> {
+public interface FacilityRepo extends JpaRepository<Facility, Long> {
 
 }
